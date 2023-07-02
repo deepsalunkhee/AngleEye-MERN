d03f4c0c311505949fe079549ebf7a284c6cec69
deepsalunkhee <99858248+deepsalunkhee@users.noreply.github.com>
2023-07-02 17:17:52 +0530

    3

--- Server/DB.js
+++ Server/DB.js
@@ -1,12 +1,14 @@
 const mongoose =require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.Mongo_key,{
-    useNewUrlParser:true,
-    //q:what does above line do?
-    //a:it is used to parse the url string in the connect method
-    useUnifiedTopology:true,
-})
+mongoose.connect(process.env.Mongo_key, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    // Continue with your server setup or other operations
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 const db=mongoose.connection;
 db.on('error',console.error.bind(console,'DB connection failed'))
